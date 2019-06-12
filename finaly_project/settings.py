63255ab99779e2170511865843c6f625fea6d39f
Alymbekov <maxim.makarov.1997@mail.ru>
2019-06-12 18:24:14 +0600

    ff

--- finaly_project/settings.py
+++ finaly_project/settings.py
@@ -78,16 +78,22 @@ WSGI_APPLICATION = 'finaly_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'postgres',
+#         'USER': 'postgres',
+#         'HOST': 'db',
+#         'PORT': 5432,
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'HOST': 'db',
-        'PORT': 5432,
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 AUTH_USER_MODEL = 'imaster.User'
 
 # Password validation
